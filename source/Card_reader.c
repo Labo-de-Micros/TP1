ebcf8e3aea97b36e4af42796b67b169cc63764d6
idiaz97 <idiaz@itba.edu.ar>
2020-09-25 14:20:11 -0300

    Sumados checkeos
    Se sumaron chequeos de compilacion y mas defines para hacer el codigo mas portable , se pueden cambiar los tamaños de los registros.

--- source/Card_reader.c
+++ source/Card_reader.c
@@ -63,7 +63,7 @@ typedef struct{
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-static uint32_t card_data_compressed[CARD_DATA_LENGTH_32];
+static uint32_t card_data_compressed[CARD_DATA_REGISTERS_LENGTH];
 static uint8_t index;
 static card_callback_t	call;
 static card_states_t current_state;
@@ -85,7 +85,6 @@ static bool is_char_valid(card_char_t character);
 static uint8_t search_for_start(void);
 static void clear_card(void);
 
-
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //					FUNCTION DEFINITIONS						//
@@ -97,25 +96,23 @@ void card_init(card_callback_t callback){
  * @brief: Initializer the card reader driver and its components
  * @param callback: callback to be called when a new card is readed.
  ****************************************************************/
-	gpioMode(CARD_DATA, INPUT);
+	gpioMode(CARD_DATA, INPUT);	// Inicializo los pines correspondientemente
 	gpioMode(CARD_CLOCK, INPUT);
 	gpioMode(CARD_ENABLE, INPUT);
-	gpioIRQ (CARD_CLOCK, GPIO_IRQ_MODE_FALLING_EDGE , clock_callback);	//Asumo que el clock no tirara nada cuando no haya leido nada.
+	gpioIRQ (CARD_CLOCK, GPIO_IRQ_MODE_FALLING_EDGE , clock_callback);	//Inicializo las interrupciones
 	gpioIRQ (CARD_ENABLE, GPIO_IRQ_MODE_BOTH_EDGES , enable_callback);
 	call = callback;
-	current_state = CARD_WAIT_FOR_START;
+	current_state = CARD_WAIT_FOR_START;	//Seteo el estado de la maquina de estados como 'esperando targeta'.
 	index = 0;
-	clear_buffer();
+	clear_buffer();	// Limpio el buffer para que todo quede seteado en 0.
 	return;
 }
 
 card_t get_data(void){
 /*****************************************************************
  * @brief: When the callback is called, one must obtain the data readed
- * 			by the driver, so this function returns the data.
- * 			(It must be called in the callback specified above).
- * @return: An array of lenght CARD_CHARACTERS_LENGTH containing the data
- * 			readed in the form of uint8_t.
+ * 			by the driver, so this function returns the card data.
+ * @return: A struct card_t containing the information readed in the card.
  ****************************************************************/
 //Por ahora asumo que siempre la targeta se lee en la direccion correcta.
 	clear_card();
@@ -129,6 +126,7 @@ card_t get_data(void){
 			card.pan = card.pan * 10 + num;
 		}
 	}
+
 	num = get_char_num(get_current_char(start_index+i*5));
 	i++;
 	card.exp_year = num*10 + get_char_num(get_current_char(start_index+i*5));
@@ -155,9 +153,9 @@ card_t get_data(void){
 		card.CVV = card.CVV*10 +  num;
 	}
 	i++;
+
 	return card;
 }
-
 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
@@ -170,25 +168,27 @@ static void card_machine(card_events_t ev){
  * @brief: Runs the state machine for the card reader protocol
  * 			(ISO/IEC 7811-2)
  ****************************************************************/
-	uint8_t data = gpioRead(CARD_DATA);	//reads the data pin from the card reader.
+	uint8_t data = gpioRead(CARD_DATA);	//Leo el dato del pin apenas entra a la funcion para que no me afecte el tiempo
+										// de ejecucion de las instrucciones.
 	switch(current_state){
-		case CARD_WAIT_FOR_START:	// Estado inicial, si me llega un Falling de enable, esta leyendo targeta.
-			if(ev == ENABLE_FALLING_EV){
+		case CARD_WAIT_FOR_START:		// Estado inicial, esperando a que se empieze a leer una targeta.
+			if(ev == ENABLE_FALLING_EV){// Si llega un flanco negativo del pin ENABLE, se empezo a leer una targeta
+										// entonces pasamos al estado WAIT_DATA.
 				current_state = CARD_WAIT_DATA;
 				index = 0;
 				clear_buffer();
 			}
 			break;
-		case CARD_WAIT_DATA:
-			if(ev == CLK_EV){
-				if(data == 0)
-					card_data_compressed[index/32] |= CARD_32_MASK << (31-(index % 32));
-				index++;
+		case CARD_WAIT_DATA:	// Estado de espera de arribo de nuevo dato.
+			if(ev == CLK_EV){	// Si llega un flanco negativo de clock, guardo el dato leido.
+				if(data == 0)	// Es importante notar que el dato leido en el pin esta negado, por eso este data == 0.
+								// ya que en caso de que asi sea, escribo un 1 en el lugar correspondiente de los 8 registros de 32 bits.
+					card_data_compressed[index/CARD_DATA_REGISTER] |= CARD_32_MASK << ((CARD_DATA_REGISTER-1)-(index % CARD_DATA_REGISTER));
+				index++;		// incremento el indice del dato actual.
 			}
-			else if(ev == ENABLE_RISING_EV){
-				call();
-				get_current_char(15);
-				current_state = CARD_WAIT_FOR_START;
+			else if(ev == ENABLE_RISING_EV){		// Si llega un flanco positivo del pin ENABLE, se termino de leer la targeta
+				call();								// entonces llamo al callback, y seteo el estado como el estado inicial, por si se lee
+				current_state = CARD_WAIT_FOR_START;// otra trageta.
 			}
 			break;
 		default:
@@ -221,7 +221,7 @@ static void clear_buffer(void){
 /*****************************************************************
  * @brief: Clears the 'card_data_compressed' buffer
  ****************************************************************/
-	for(uint16_t i = 0; i < CARD_DATA_LENGTH_32; i++)
+	for(uint16_t i = 0; i < CARD_DATA_REGISTERS_LENGTH; i++)
 		card_data_compressed[i] = 0;
 	index = 0;
 	return;
@@ -242,17 +242,22 @@ static card_char_t get_current_char(uint8_t ind){
  * 					data_p = 0
  ****************************************************************/
 	card_char_t curr_char;
-	uint8_t i = ind %32;
-	uint8_t index_2 = ind/32;
-	curr_char.data_0 = (card_data_compressed[index_2] & (CARD_32_MASK<<(31-i))) != 0;
+	uint8_t i = ind % CARD_DATA_REGISTER;      // indice de que bit tengo que agarrar.
+	uint8_t index_2 = ind/ CARD_DATA_REGISTER; // Indice de que registro tengo que agarrar.
+	// Agarro el registro correspondiente y realizo una operacion AND con un 1 ubicado en
+	// la posición i-esima. Finalmente me fijo si el numero resultante es 0 o no, en caso
+	// de que no sea cero, guardo un 1, caso contrario un 0.
+	// Recordemos que la posición 0 es el bit mas significativo y la posicion 31 (para el caso de
+	// registros de 32 bits) es el bit menos significativo.
+	curr_char.data_0 = (card_data_compressed[index_2] & (CARD_32_MASK<<((CARD_DATA_REGISTER-1)-i))) != 0;
 	i++;
-	curr_char.data_1 = (card_data_compressed[index_2] & (CARD_32_MASK<<(31-i))) != 0;
+	curr_char.data_1 = (card_data_compressed[index_2] & (CARD_32_MASK<<((CARD_DATA_REGISTER-1)-i))) != 0;
 	i++;
-	curr_char.data_2 = (card_data_compressed[index_2] & (CARD_32_MASK<<(31-i))) != 0;
+	curr_char.data_2 = (card_data_compressed[index_2] & (CARD_32_MASK<<((CARD_DATA_REGISTER-1)-i))) != 0;
 	i++;
-	curr_char.data_3 = (card_data_compressed[index_2] & (CARD_32_MASK<<(31-i))) != 0;
+	curr_char.data_3 = (card_data_compressed[index_2] & (CARD_32_MASK<<((CARD_DATA_REGISTER-1)-i))) != 0;
 	i++;
-	curr_char.data_p = (card_data_compressed[index_2] & (CARD_32_MASK<<(31-i))) != 0;
+	curr_char.data_p = (card_data_compressed[index_2] & (CARD_32_MASK<<((CARD_DATA_REGISTER-1)-i))) != 0;
 	return curr_char;
 }
 
@@ -288,6 +293,7 @@ static uint8_t search_for_start(void){
  * @brief: Search for the start of the Card Sequence.
  * @returns: uint8_t index of the start of the card sequence.
  ****************************************************************/
+	// Busco que indice se corresponde con el caracter SS (inicio de targeta).
 	uint8_t i = 0;
 	for(uint8_t ind = 0 ; ind < CARD_DATA_LENGTH ; ind++){
 		if(get_char_num(get_current_char(ind)) == CARD_SS && is_char_valid(get_current_char(ind))){
@@ -295,10 +301,14 @@ static uint8_t search_for_start(void){
 			break;
 		}
 	}
+	// Una vez que lo encuentro, devuelvo la posicion del siguiente caracter.
 	return i+5;
 }
 
 static void clear_card(void){
+/*****************************************************************
+ * @brief: Clears all data from the variable card.
+ ****************************************************************/
 	card.pan = 0;
 	card.CVV = 0;
 	card.exp_month = 0;
--- source/Card_reader.h
+++ source/Card_reader.h
@@ -24,15 +24,24 @@
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-#define CARD_DATA_LENGTH	256	//bits en la targeta, midiendolo llegue a que eran 256
-#define CARD_DATA_LENGTH_32	(CARD_DATA_LENGTH/32)
-#define CARD_CHARACTERS_LENGTH	40 //Segun la consigna.
-#define CARD_PAN_LENGHT			19
-#define	CARD_EXP_LENGHT			4
-#define CARD_SERVICE_LENGHT		3
-#define CARD_PVKI_LENGHT		1
-#define CARD_PVV_LENGHT			4
-#define CARD_CVV_LENGHT			3
+#define CARD_DATA_LENGTH		256		//Bits totales en la targeta, midiendolo llegue a que eran 256
+#define CARD_DATA_REGISTER		32		//Uso registros de 32 bits para guardar la data.
+#define CARD_CHARACTERS_LENGTH	40		//Segun la consigna.
+#define CARD_PAN_LENGHT			19		//Maxima cantidad de caracteres de PAN
+#define	CARD_EXP_LENGHT			4		//Maxima cantidad de caracteres de expiration date (YYMM)
+#define CARD_SERVICE_LENGHT		3		//Maxima cantidad de caracteres de Service Code
+#define CARD_PVKI_LENGHT		1		//Maxima cantidad de caracteres de PVKI
+#define CARD_PVV_LENGHT			4		//Maxima cantidad de caracteres de PVV
+#define CARD_CVV_LENGHT			3		//Maxima cantidad de caracteres de CVV
+#define CARD_DATA_REGISTERS_LENGTH	(CARD_DATA_LENGTH/CARD_DATA_REGISTER)	//Cantidad de registros de 32 bits necesarios para guardar la CARD_DATA_LENGTH
+													//IMPORTANTE: es necesario que CARD_DATA_LENGTH sea multiplo de 32. (No sirve otra cosa).
+#if ((CARD_DATA_LENGTH%CARD_DATA_REGISTER) != 0)
+#error CARD_DATA_LENGTH must be multiple of CARD_DATA_REGISTER! Cannot be otherwise! Please change CARD_DATA_LENGTH or CARD_DATA_REGISTER.
+#endif
+
+#if (CARD_PAN_LENGHT+CARD_EXP_LENGHT+CARD_EXP_LENGHT+CARD_SERVICE_LENGHT+CARD_PVKI_LENGHT+CARD_PVV_LENGHT+CARD_CVV_LENGHT) > CARD_CHARACTERS_LENGTH
+#error The ammount of characters per card specified is not correct. Please check the defines:CARD_PAN_LENGHT, CARD_EXP_LENGHT, CARD_SERVICE_LENGHT, CARD_PVKI_LENGHT, CARD_PVV_LENGHT, CARD_CVV_LENGHT and CARD_CHARACTERS_LENGTH.
+#endif
 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
@@ -67,11 +76,8 @@ void card_init(card_callback_t callback);
 card_t get_data(void);
 /*****************************************************************
  * @brief: When the callback is called, one must obtain the data readed
- * 			by the driver, so this function returns the data.
- * 			(It must be called in the callback specified above).
- * @return: An array of lenght CARD_DATA_LENGTH containing the data
- * 			readed.
+ * 			by the driver, so this function returns the card data.
+ * @return: A struct card_t containing the information readed in the card.
  ****************************************************************/
-
 
 #endif	//_CARD_READER_H_
