8baa2e6a7830bb92e5ca6ec28d204e277a903318
idiaz97 <idiaz@itba.edu.ar>
2020-10-15 21:14:43 -0300

    Subi los testers q borre

---
+++ source/Testers/App_pwm.c
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+#include "board.h"
+#include "hardware.h"
+
+#include "FTM.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+#define DEBUG_OSCILLOSCOPE
+#define BUTTON_CHECK_MS			10
+#define BLINK_FREQ_HZ 			2U
+#define BUTTON_CHECK_TICKS	TIMER_MS2TICKS(10)
+#define BLINK_FREQ_TICKS 		TIMER_MS2TICKS( 1000/BLINK_FREQ_HZ )
+
+#if SYSTICK_ISR_FREQUENCY_HZ % (2*BLINK_FREQ_HZ) != 0
+#warning BLINK cannot implement this exact frequency.
+		Using floor(SYSTICK_ISR_FREQUENCY_HZ/BLINK_FREQ_HZ/2) instead.
+#endif
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+void App_Init (void) {
+
+ 	 	 	 	hw_Init ();
+ 	 	 		FTM_Init(FTM0);
+	return;
+}
+
+void App_Run (void){
+    while (true)
+    {
+
+	}
+	return;
+}
