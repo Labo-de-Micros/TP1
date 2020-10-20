1b9ed1831011705c456def8208690e55d8827d58
idiaz97 <idiaz@itba.edu.ar>
2020-10-20 13:20:52 -0300

    Update board.h

--- source/board.h
+++ source/board.h
@@ -1,63 +1,61 @@
-/***************************************************************************//**
-  @file     board.h
-  @brief    Board management
-  @author   Nicolás Magliola
- ******************************************************************************/
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//	@file		board.h										    //
+//	@brief		Board Management								//
+//	@author		Grupo	4										//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
 #ifndef _BOARD_H_
 #define _BOARD_H_
 
-/*******************************************************************************
- * INCLUDE HEADER FILES
- ******************************************************************************/
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//							Headers								//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
 #include "./Drivers/GPIO/gpio.h"
 
 
-/*******************************************************************************
- * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
- ******************************************************************************/
-
-/***** BOARD defines **********************************************************/
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//		CONSTANT AND MACRO DEFINITIONS USING #DEFINE 		 	//
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
 // On Board User LEDs
-#define PIN_LED_RED     	PORTNUM2PIN(PB,22) 	// PTB22
-#define PIN_LED_GREEN   	PORTNUM2PIN(PE,26)	// PTB26
-#define PIN_LED_BLUE    	PORTNUM2PIN(PB,21) 	// PTB21
-
-#define INTERRUPT_PIN		PORTNUM2PIN(PC,5)		//PTC5
-#define DEBUG_PIN			PORTNUM2PIN(PE,25)		//PTE25
+#define PIN_LED_RED     	PORTNUM2PIN(PB,22) 	//PTB22
+#define PIN_LED_GREEN   	PORTNUM2PIN(PE,26)	//PTB26
+#define PIN_LED_BLUE    	PORTNUM2PIN(PB,21) 	//PTB21
+// Debug Pins
+#define INTERRUPT_PIN		PORTNUM2PIN(PC,5)	//PTC5
+#define DEBUG_PIN			PORTNUM2PIN(PE,25)	//PTE25
 #define SIGNAL_PIN			PORTNUM2PIN(PC,12)	//PTC12
-
+// Encoder Pins
 #define ENCODER_PIN_A   	PORTNUM2PIN(PC,5)	//PTC5
-#define ENCODER_PIN_B   	PORTNUM2PIN(PC,17)//PTC17
-#define ENCODER_PIN_SW  	PORTNUM2PIN(PC,16)//PTC16
-
-#define DISPLAY_PIN_A   	PORTNUM2PIN(PC,7)
-#define DISPLAY_PIN_B   	PORTNUM2PIN(PC,0)
-#define DISPLAY_PIN_C   	PORTNUM2PIN(PC,9)
-#define DISPLAY_PIN_D   	PORTNUM2PIN(PC,8)
-#define DISPLAY_PIN_E   	PORTNUM2PIN(PD,0)
-#define DISPLAY_PIN_F   	PORTNUM2PIN(PC,12)
-#define DISPLAY_PIN_G   	PORTNUM2PIN(PC,4)
-#define DISPLAY_MUX_PIN_0   PORTNUM2PIN(PA,1)
-#define DISPLAY_MUX_PIN_1   PORTNUM2PIN(PB,9)
-
-#define FTM_PIN         	PORTNUM2PIN(PC,1)
-
-#define CARD_DATA   		PORTNUM2PIN(PB,2)
-#define CARD_CLOCK  		PORTNUM2PIN(PB,3)
-#define CARD_ENABLE 		PORTNUM2PIN(PB,10)
-
-
+#define ENCODER_PIN_B   	PORTNUM2PIN(PC,17)	//PTC17
+#define ENCODER_PIN_SW  	PORTNUM2PIN(PC,16)	//PTC16
+// Display pins
+#define DISPLAY_PIN_A   	PORTNUM2PIN(PC,7)	//PTC7
+#define DISPLAY_PIN_B   	PORTNUM2PIN(PC,0)	//PTC0
+#define DISPLAY_PIN_C   	PORTNUM2PIN(PC,9)	//PTC9
+#define DISPLAY_PIN_D   	PORTNUM2PIN(PC,8)	//PTC8
+#define DISPLAY_PIN_E   	PORTNUM2PIN(PD,0)	//PTD0
+#define DISPLAY_PIN_F   	PORTNUM2PIN(PC,12)	//PTC12
+#define DISPLAY_PIN_G   	PORTNUM2PIN(PC,4)	//PTC4
+#define DISPLAY_MUX_PIN_0   PORTNUM2PIN(PA,1)	//PTA1
+#define DISPLAY_MUX_PIN_1   PORTNUM2PIN(PB,9)	//PTB9
+// FTM Debug Pin
+#define FTM_PIN         	PORTNUM2PIN(PC,1)	//PTC1
+// Card reader pins
+#define CARD_DATA   		PORTNUM2PIN(PB,2)	//PTB2
+#define CARD_CLOCK  		PORTNUM2PIN(PB,3)	//PTB3
+#define CARD_ENABLE 		PORTNUM2PIN(PB,10)	//PTB10
 
 #define LED_ACTIVE			LOW
-
 // On Board User Switches
-#define PIN_SW2         PORTNUM2PIN(PC,6)		// PTC6
-#define PIN_SW3         PORTNUM2PIN(PA,4)		// PTA4
-
-/*******************************************************************************
- ******************************************************************************/
+#define PIN_SW2         	PORTNUM2PIN(PC,6)	//PTC6
+#define PIN_SW3         	PORTNUM2PIN(PA,4)	//PTA4
 
 #endif // _BOARD_H_
